62c2503339e81b29b0589b8e5c5f017fac57a2e9
podiumdesu <wwhmomo@gmail.com>
2017-09-25 16:02:19 +0800

    first commit

--- src/js/display.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by petnakanojo on 06/09/2017.
- */
-require('../css/common.css');
-
-var video = document.getElementById('video');
-video.addEventListener("touchend",function(){
-    video.play();
-},false);
--- src/js/index.js
+++ src/js/index.js
@@ -1,95 +1,244 @@
-require('../css/index.css');
-require('../css/common.css');
-import {data} from './data';
+require("../css/index.css");
+require("../css/animation.css");
+require("../css/ball.css");
+require("../css/select.css");
 
-//console.log("hello")
+console.log("hello,world");
 
-const recordBtn = $("#record-btn");
-const submitBtn = $("#submit-btn");
-const text = $("#time-record");
-const waiting = $("#waiting-area");
-const container = $("#container");
-const textRead = $("#text-read");
 
-let s = Math.random() * 10;
-textRead.html(data[parseInt(s)]);
-let t;
-let c = 0;
-const sendData = {id: 1};  //上传数字给服务器，判断是否需要加载视频
-function timedCount() {
-    //console.log("Now it is starting");
-    c = c + 1;
-    if (c < 10) {
-        text.html("00:0"+c);
-    } else if (c >= 60 && c < 70) {
-        text.html("01:0"+c-60);
+const submitBtn = $("#progress-button");
+const boySel = $("#boy-sel");
+const girlSel = $("#girl-sel");
+const webSel = $("#web-sel");
+const itSel = $("#it-sel");
+const androidSel = $("#android-sel");
+const iosSel = $("#ios-sel");
+let name_sel_value;
+let sex_sel_value;
+let class_sel_value, phone_sel_value, qq_sel_value, intro_sel_value;
+let team_sel_value;
+let allRegData;
+
+submitBtn.bind("touchstart", test);
+
+
+function test() {
+    submitBtn.addClass("bounce");
+    setTimeout(function() {
+        submitBtn.removeClass("bounce");
+    }, 1000);
+    name_sel_value = $("#reg-name").val();
+    class_sel_value = $("#reg-class").val();
+    phone_sel_value = $("#reg-phone").val();
+    qq_sel_value = $("#reg-qq").val();
+    intro_sel_value = $("#reg-intro").val();
+    /*
+    console.log(name_sel_value);
+    console.log(class_sel_value);
+    console.log(sex_sel_value);
+    console.log(phone_sel_value);
+    console.log(qq_sel_value);
+    console.log(intro_sel_value);
+    console.log(team_sel_value);
+    */
+    if (name_sel_value && class_sel_value && phone_sel_value && qq_sel_value && intro_sel_value && team_sel_value && sex_sel_value) {
+        $("#form-container").addClass("toBlur");
+        submitBtn.unbind("touchstart");
+        submitBtn.addClass("toBlur");
+        $("#waiting-area").removeClass("toHide");
+
+        event = event || window.event;
+        if (event.keyCode === 13) {
+            if (searchInfo.length !== 0) {
+                ajax.open("POST","/api/signup/submit",true);
+                ajax.setRequestHeader("Content-type","application/json");
+                ajax.onreadystatechange= function() {
+                    console.log(this.readyState);
+                    if (this.readyState === 4) {   //Todo
+                        console.log(this.responseText);
+                        if (JSON.parse(this.responseText).status === "success") {
+                            alert("报名成功！！！请等待短信通知～");
+                        }
+                    }
+                };
+                allRegData.name = name_sel_value;
+                allRegData.sex = name_sel_value;
+                allRegData.class = class_sel_value;
+                allRegData.phone = phone_sel_value;
+                allRegData.qq = qq_sel_value;
+                allRegData.intro = intro_sel_value;
+                allRegData.team = team_sel_value;
+                //这里要向服务器发送请求。
+                ajax.send(JSON.stringify(allRegData));
+            }else {
+            }
+        }
     } else {
-        text.html("00:"+c);
+        alert("你还有信息没有填写完整哦～");
     }
-    t = setTimeout(timedCount, 1000);
+
 }
 
-recordBtn.bind("touchstart",function() {
-    //console.log("i am clicked");
-    if (recordBtn.hasClass("toRecord") > 0){
-        //console.log("This is stoped");
-        recordBtn.removeClass("toRecord");
-        recordBtn.html("重新录制");
-        clearTimeout(t);
-    } else {
-        c = 0;
-        recordBtn.addClass("toRecord");
-        recordBtn.html("点击停止");
-        timedCount();
+
+boySel.bind("touchstart", () => select(boySel, "selected") );
+girlSel.bind("touchstart", () => select(girlSel,"selected") );
+webSel.bind("touchstart",() => select(webSel, "selected") );
+androidSel.bind("touchstart",() => select(androidSel, "selected") );
+itSel.bind("touchstart",() => select(itSel, "selected") );
+iosSel.bind("touchstart",() => select(iosSel, "selected") );
+
+
+function select(targetSel, className) {
+    if (!targetSel.hasClass(className)) {
+        targetSel.addClass(className);
+        if (targetSel.hasClass("sex-sel")) {
+            sex_sel_value = targetSel.attr("data-id");
+            console.log(sex_sel_value);
+        } else {
+            team_sel_value = targetSel.text();
+            console.log(team_sel_value);
+        }
+
     }
-});
+    if (targetSel.siblings().hasClass(className)) {
+        targetSel.siblings().removeClass(className);
+    }
 
-submitBtn.bind("touchstart",function() {
-    if (c === 0) {
-        text.html("请录音");
-        setTimeout(function() {
-            text.html("00:00");
-        },2000);
+ }
+
+
+window.onload = function() {
+    var container = document.getElementById("container");
+
+    var cW = container.offsetWidth;
+    var cH = container.offsetHeight;
+    var gravity = 3.00;
+    var lifespan1 = 100;
+    var lifespan2 = 150;
+    var ground = .3 * cH;
+    var startX;
+    var r = 38;
+    var speedX;
+    var speedYDown = 3;
+    var speedYUp = 15;
+    var fontSize = 30;
+    if (cW > 500) {
+        startX = 0.25 * cW;
+        speedX = 0.005 * cW;
     } else {
-        //用户跳过停止，直接点击提交处理
-        recordBtn.removeClass("toRecord");
-        recordBtn.html("重新录制");
-        clearTimeout(t);
-        submitBtn.addClass('bounce');
-        setTimeout(function () {
-            submitBtn.removeClass('bounce');
-            waiting.removeClass("toHide");
-            $("#text-area").addClass("toFilter");
-            recordBtn.attr("disabled","disabled");
-            submitBtn.attr("disabled","disabled");
-            //解绑事件
-            recordBtn.unbind("touchstart");
-            submitBtn.unbind("touchstart");
-        },1000);
+        startX = 0.15 * cW;
+        speedX = 0.007 * cW;
+    }
 
-        //window.location.href = './display.html';
-        //Ajax与服务器通信
-        console.log(sendData);
-        let ajax = new XMLHttpRequest();
-        ajax.open("POST","/check.php",true);
-        ajax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-        ajax.onreadystatechange = function() {
-            console.log(this.readyState);
-            if (this.readyState === 4) {
-                console.log(this.responseText);
-                console.log(JSON.parse(this.responseText).status);
-                if (JSON.parse(this.responseText).status === "yes") {
-                    let time = Math.random() * 20;
-                    time = parseInt(time) + 50;
-                    setTimeout(function() {
-                        window.location.href = './display.html';
-                    }, time * 1000);
-                    //displayVideo();
-                }
+
+    // Ball object
+    var Ball = function(sLetter, index) {
+        this.sLetter = sLetter;
+        this.node;
+        this.x = startX;
+        this.y = ground - 50;
+        this.index = index;
+        this.r = r;
+        this.jumpN = 0;
+        this.speedY = speedYDown;
+        this.speedX = speedX;
+        this.opa = 1;
+        this.create();
+    }
+
+    Ball.prototype = {
+        create: function() {
+            this.node = document.createElement("div");
+            this.node.className = "ball";
+            this.node.style.width = this.r + "px";
+            this.node.style.height = this.r + "px";
+            this.node.style.left = this.x + "px";
+            this.node.style.top = this.y + "px";
+            this.node.innerHTML = this.sLetter;
+            container.appendChild(this.node);
+            this.node.style.fontSize = fontSize + "px";
+        },
+        move: function() {
+            this.y += this.speedY;
+            this.x += this.speedX;
+
+        },
+        display: function() {
+            //this.node.style.transform = "translate("+ this.x + "px," + this.y + "px)";
+            //this.node.style.top = this.y + "px";
+            //this.node.style.left = this.x + "px";
+            this.node.style.top = this.y / cH * 100 + "%";
+            this.node.style.left = this.x / cW * 100 + "%";
+        }
+    }
+
+
+    // TextBall object
+    var TextBalls = function(sText) {
+        this.sText = sText + " ";
+        this.n = sText.length + 1;
+        this.balls = [];
+        this.timeIntv = null;
+        this.life = 0;
+        this.createBalls();
+
+    }
+    TextBalls.prototype = {
+        createBalls: function() {
+            for (var i = 0; i < this.n; i++) {
+                var ball = new Ball(this.sText[i], i);
+                this.balls.push(ball);
             }
-        };
-        ajax.send(JSON.stringify(sendData));
+            this.balls[this.n-1].node.className = "cover";
+            this.balls[this.n-1].xTarget = cW;
+        },
 
+        move: function() {
+            var thisObj = this;
+            this.timeIntv = setInterval(function(){
+                thisObj.life++;
+                if (thisObj.life < lifespan2) {
+                    for (var i = 0; i < thisObj.n; i++) {
+                        var ball = thisObj.balls[i];
+                        if (ball.y < ground) {
+                            ball.speedY += gravity;
+                        } else {
+                            ball.y = ground;
+                            if (ball.jumpN < i || i == thisObj.n - 1) {
+                                ball.jumpN++;
+                                ball.speedY = - speedYUp;
+                            } else {
+                                ball.speedY = 0;
+                                ball.speedX = 0;
+                            }
+                        }
+
+                        ball.move();
+                        ball.display();
+
+                    }
+                    if (thisObj.life > lifespan1) {
+                        var coverB1 = thisObj.balls[thisObj.n-1];
+                        coverB1.opa = coverB1.opa > 0? coverB1.opa-0.025 : 0;
+                        coverB1.node.style.opacity = coverB1.opa;
+
+                    }
+                }  else {clearInterval(thisObj.timeIntv);}
+
+            }, 50);
+
+        }
     }
-});
 
+    var tb = new TextBalls("WELCOME..");
+    tb.move();
+
+
+}
+
+setTimeout(function() {
+    $("#container").addClass("toHide");
+    setTimeout(function() {
+        $("#reg-page").removeClass("toHide");
+    }, 100);
+},7000);
